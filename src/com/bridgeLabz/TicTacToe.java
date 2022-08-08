6e6f1ad2aeb42362441673c5bfa86dbae4efe403
KetanC1211 <ketanchindarkar12@gmail.com>
2022-08-08 20:30:20 +0530

    Added method to choose X and 0

--- src/com/bridgeLabz/TicTacToe.java
+++ src/com/bridgeLabz/TicTacToe.java
@@ -1,7 +1,8 @@
 package com.bridgeLabz;
+import java.util.Scanner;
 
 public class TicTacToe {
-	public static char gameboard [] = new char[10]; 
+	public static char gameBoard [] = new char[10]; 
 	
 	public static void displayBoard(char gameBoard[]) {
 		for(int i=1;i<10;i++) {
@@ -17,13 +18,49 @@ public class TicTacToe {
 		System.out.println("|");
 		System.out.println("-------------");
 	}
-	
+
+	public static void addXand0inBox(char enteredVal, int position) {
+		if(gameBoard[position]!=' ') {
+			play();
+		}
+		gameBoard[position] = enteredVal;
+		displayBoard(gameBoard);
+	}
+
+	public static void play() {
+		Scanner scan = new Scanner(System.in);
+		int toss = (int)(Math.random()*10 %2);
+		int n = 1;
+		while( n <= 9) {
+			switch(toss) {
+				case 1:
+					System.out.println("Enter the position to place X");
+					int positionX = scan.nextInt();
+					addXand0inBox('X',positionX);				
+					break;
+				case 0:
+					System.out.println("Enter the position to place 0");
+					//int position0 = scan.nextInt();
+					addXand0inBox('0',(int)(Math.random()*10 %9 +1));
+					break;
+			}
+			if(toss==1) {
+				toss=0;
+			}
+			else {
+				toss=1;
+			}
+			n++;
+		}
+	}
+
 	public static void main(String[] args) {	
 		System.out.println("Welcomw to Tic-Tac-Toe");
 		for(int i=0;i<10;i++) {
-			gameboard [i] = ' ' ;
+			gameBoard [i] = ' ' ;
 		}
 		System.out.println();
-		displayBoard(gameboard);
+		displayBoard(gameBoard);
+		play();
 	}		
 }
