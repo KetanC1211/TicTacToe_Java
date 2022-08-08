7ed103432651ce24de0e35517d27bf7d37c7aad7
KetanC1211 <ketanchindarkar12@gmail.com>
2022-08-08 20:46:02 +0530

    CheckForFreeSpace

--- src/com/bridgeLabz/TicTacToe.java
+++ src/com/bridgeLabz/TicTacToe.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	public static char gameBoard [] = new char[10]; 
+	static int tossResultForPlayer = (int)(Math.random()*10 %2);
+	static int tossResultForComputer;
 	
 	public static void displayBoard(char gameBoard[]) {
 		for(int i=1;i<10;i++) {
@@ -20,36 +22,66 @@ public class TicTacToe {
 	}
 
 	public static void addXand0inBox(char enteredVal, int position) {
-		if(gameBoard[position]!=' ') {
-			play();
-		}
 		gameBoard[position] = enteredVal;
 		displayBoard(gameBoard);
 	}
 
+	public static void playFromPlayerSide() {
+		Scanner sc = new Scanner(System.in);
+		switch(tossResultForPlayer) {//if he gets 1 he will go with X or 0
+			case 1:
+				System.out.println("Enter Player position to place X");
+				int positionX = sc.nextInt();
+				if(gameBoard[positionX]!=' ') {
+					System.out.println("Space not empty please enter vaild position");
+					playFromPlayerSide();
+					break;
+				}
+				addXand0inBox('X',positionX);				
+				break;
+			case 0:
+				System.out.println("Enter Player position to place 0");
+				int position0 = sc.nextInt();
+				if(gameBoard[position0]!=' ') {
+					System.out.println("Space not empty please enter vaild position");
+					playFromPlayerSide();
+					break;
+				}
+				addXand0inBox('0',position0);
+				break;
+		}
+	}
+
+	public static void playFromComputerSide() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Computer Turn's");
+		switch(tossResultForComputer) {			
+			case 1:
+				int positionX =(int)(Math.random()*10 %9 +1);
+				if(gameBoard[positionX]!=' ') {
+					System.out.println("Space not empty please enter vaild position");
+					playFromComputerSide();
+					break;
+				}
+				addXand0inBox('X',positionX);				
+				break;
+			case 0:
+				int position0 =(int)(Math.random()*10 %9 +1);
+				if(gameBoard[position0]!=' ') {
+					System.out.println("Space not empty please enter vaild position");
+					playFromComputerSide();
+					break;
+				}
+				addXand0inBox('0',position0);
+				break;
+		}
+	}
+
 	public static void play() {
-		Scanner scan = new Scanner(System.in);
-		int toss = (int)(Math.random()*10 %2);
-		int n = 1;
-		while( n <= 9) {
-			switch(toss) {
-				case 1:
-					System.out.println("Enter the position to place X");
-					int positionX = scan.nextInt();
-					addXand0inBox('X',positionX);				
-					break;
-				case 0:
-					System.out.println("Enter the position to place 0");
-					//int position0 = scan.nextInt();
-					addXand0inBox('0',(int)(Math.random()*10 %9 +1));
-					break;
-			}
-			if(toss==1) {
-				toss=0;
-			}
-			else {
-				toss=1;
-			}
+		int n=1;
+		while(n<=50) {
+			playFromPlayerSide();
+			playFromComputerSide();
 			n++;
 		}
 	}
@@ -59,7 +91,11 @@ public class TicTacToe {
 		for(int i=0;i<10;i++) {
 			gameBoard [i] = ' ' ;
 		}
-		System.out.println();
+		if(tossResultForPlayer==1) {
+			tossResultForComputer=0;
+		}else {
+			tossResultForComputer=1;
+		}
 		displayBoard(gameBoard);
 		play();
 	}		
