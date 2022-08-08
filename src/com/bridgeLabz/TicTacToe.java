1b4fa0f0d8eda720135d1242a2e3242725a83416
KetanC1211 <ketanchindarkar12@gmail.com>
2022-08-08 22:47:31 +0530

    Toss and play according

--- src/com/bridgeLabz/TicTacToe.java
+++ src/com/bridgeLabz/TicTacToe.java
@@ -3,8 +3,45 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	public static char gameBoard [] = new char[10]; 
-	static int tossResultForPlayer = (int)(Math.random()*10 %2);
+	static int tossToDecideWhoStartsFirst = (int)(Math.random()*10 %2);
 	static int tossResultForComputer;
+	static int playerChoiceForXand0;
+	static int computerChoiceForXand0;
+	static int playerChoiceForToss;
+
+	public static void tossBetweenPlayerAndComputer() {
+		System.out.println("lets to do Toss");
+		System.out.println("To select heads enter 1 \nTo select tails enter 0");
+		Scanner sc = new Scanner(System.in);
+		playerChoiceForToss = sc.nextInt();
+		 
+		if(tossToDecideWhoStartsFirst==playerChoiceForToss) {
+			System.out.println("You won the toss you can play first");
+			System.out.println("To play with X enter 1 \nTo play with 0 enter 0");
+			playerChoiceForXand0 = sc.nextInt();
+			if(playerChoiceForXand0==1) {
+				System.out.println("You have selected X to start Game \nComputer will play with 0");
+				computerChoiceForXand0=0;
+			}
+			else {
+				System.out.println("You have selected 0 to start game \nComputer will play with X");
+				computerChoiceForXand0=1;
+			}
+		}
+		else {
+			System.out.println("You lost the toss computer will play first");
+			computerChoiceForXand0 = (int)(Math.random()*10 %2);
+			if(computerChoiceForXand0==1) {
+				System.out.println("Computer have selected X to start Game \nYou have to will play with 0");
+				playerChoiceForXand0=0;
+			}
+			else {
+				System.out.println("Computer have selected 0 to start Game \nYou have to will play with X");
+				playerChoiceForXand0=1;
+			}
+		}
+		
+	}
 	
 	public static void displayBoard(char gameBoard[]) {
 		for(int i=1;i<10;i++) {
@@ -28,7 +65,7 @@ public class TicTacToe {
 
 	public static void playFromPlayerSide() {
 		Scanner sc = new Scanner(System.in);
-		switch(tossResultForPlayer) {//if he gets 1 he will go with X or 0
+		switch(playerChoiceForXand0) {//if he gets 1 he will go with X or 0
 			case 1:
 				System.out.println("Enter Player position to place X");
 				int positionX = sc.nextInt();
@@ -55,7 +92,7 @@ public class TicTacToe {
 	public static void playFromComputerSide() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Computer Turn's");
-		switch(tossResultForComputer) {			
+		switch(computerChoiceForXand0) {			
 			case 1:
 				int positionX =(int)(Math.random()*10 %9 +1);
 				if(gameBoard[positionX]!=' ') {
@@ -79,10 +116,19 @@ public class TicTacToe {
 
 	public static void play() {
 		int n=1;
-		while(n<=50) {
-			playFromPlayerSide();
-			playFromComputerSide();
-			n++;
+		if(tossToDecideWhoStartsFirst==playerChoiceForToss){	
+			while(n<=50) {
+				playFromPlayerSide();
+				playFromComputerSide();
+				n++;
+			}
+		}
+		else{
+			while(n<=50) {
+				playFromComputerSide();
+				playFromPlayerSide();
+				n++;
+			}
 		}
 	}
 
@@ -91,12 +137,8 @@ public class TicTacToe {
 		for(int i=0;i<10;i++) {
 			gameBoard [i] = ' ' ;
 		}
-		if(tossResultForPlayer==1) {
-			tossResultForComputer=0;
-		}else {
-			tossResultForComputer=1;
-		}
 		displayBoard(gameBoard);
+		tossBetweenPlayerAndComputer();		
 		play();
 	}		
 }
